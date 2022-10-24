27ac2f4a7e95c2733b0d86160c48f9326aa3ddde
StepaKyznetsov <kyznetsovstepa@gmail.com>
2022-10-24 13:40:15 +0300

    update

--- second-task.js
+++ second-task.js
@@ -5,83 +5,105 @@ let arr = [],
 
 readline.on('line', (line) => {
     arr.push(line.split(' ').map(s => +s));
-    const m = arr[0][0];
-    const n = arr[0][1];
-    if(i === n) {
-        let squaresArr = [];
-        let effectivityArr = [];
-        const numbers = arr.slice(1);
-        console.log(numbers)
-        let regionNumber = 2;
-        for(let i = 0; i < m; i++){
-            for(let j = 0; j < n; j++){
-                console.log(numbers[i][j])
-                if (numbers[i][j] === 1) {
-                    neighbors(numbers, m, n, i, j, regionNumber);
-                    regionNumber++;
+    const n = arr[0][0]
+    const m = arr[0][1]
+    if(i == m) {
+        const borderMap = arr.slice(1).map((arr) => [0, ...arr.slice(0, n), 0]);
+        borderMap.unshift(new Array(borderMap[0].length).fill(0));
+
+        let domainMarkCounter = 0;
+        const neighborsMap = [];
+
+        for (let y = 1; y < borderMap.length; y++){
+            for (let x = 1; x < borderMap[y].length - 1; x++){
+                let target = borderMap[y][x],
+                    left = borderMap[y][x - 1],
+                    leftTop = borderMap[y - 1][x - 1],
+                    middleTop = borderMap[y - 1][x],
+                    rightTop = borderMap[y - 1][x + 1],
+                    upperNeighbors = []
+                    upperNeighbors.push(leftTop, middleTop, rightTop);
+                if (target === 1) {
+                    if (left !== 0){
+                        borderMap[y][x] = left;
+                } else{
+                    const color = upperNeighbors.find((top) => top !== 0);
+                    borderMap[y][x] = color ? color : domainMarkCounter++;
                 }
-            }
-        }
-        let top = 0,
-            bottom = 0,
-            left = 0,
-            right = 0,
-            first = true;
-        for(let k = 2; k < regionNumber; k++){
-            for (let i = 0; i < m; i++){
-                for (let j = 0; j < n; j++){
-                    if (numbers[i][j] === k){
-                        if (first) {
-                            top = i;
-                            bottom = i;
-                            left = j;
-                            right = j;
-                            first = false;
-                        } else {
-                            top = Math.min(i, top);
-                            bottom = Math.max(i, bottom);
-                            left = Math.min(j, left);
-                            right = Math.max(j, right);
+        
+                let center = borderMap[y][x];
+                if (rightTop !== 0 && rightTop !== center){
+                    let detachedBranch = [];
+                    const detachedBranchIndex = neighborsMap.findIndex((neighbors) => neighbors.has(rightTop));
+                    if (detachedBranchIndex !== -1){
+                        if (!neighborsMap[detachedBranchIndex].has(center)){
+                            detachedBranch = neighborsMap.splice(detachedBranchIndex, 1)[0];
                         }
                     }
+        
+                    let colorsCore = neighborsMap.find((neighbors) => neighbors.has(center));
+                    if (!colorsCore) {
+                        colorsCore = new Set().add(center);
+                        neighborsMap.push(colorsCore);
+                    }
+                    colorsCore.add(rightTop);
+        
+                    [...detachedBranch].forEach((color) => colorsCore.add(color));
+                }
+              }
+            }
+        }
+        const colorAliases = neighborsMap.map(([main, ...neighbors]) => ({ main, neighbors }));
+        
+        const regions = Array(domainMarkCounter);
+        for (let y = 1; y < borderMap.length; y++){
+            for (let x = 1; x < borderMap[y].length - 1; x++){
+                const target = borderMap[y][x];
+                if (target === 0) continue;
+        
+                const colorAliase = colorAliases.find(({ neighbors }) => neighbors.includes(target));
+                const color = colorAliase ? colorAliase.main : target;
+                if (!regions[color]) {
+                    regions[color] = {
+                        minX: x,
+                        maxX: x,
+                        minY: y,
+                        maxY: y,
+                        power: 1,
+                    };
+                continue;
+                }
+                const region = regions[color];
+                region.power += 1;
+                if (region.minX > x) region.minX = x;
+                if (region.maxX < x) region.maxX = x;
+                if (region.minY > y) region.minY = y;
+                if (region.maxY < y) region.maxY = y;
+            }
+        }
+        
+        const analytics = regions
+        .filter((region) => region.power > 1)
+        .map((region) => {
+            let areaPower = 0;
+            for (let y = region.minY; y <= region.maxY; y++){
+                for (let x = region.minX; x <= region.maxX; x++){
+                    borderMap[y][x] && areaPower++;
                 }
             }
-            let fertile = 0;
-            let square = (bottom - top + 1) * (right - left + 1);
-            for (let i = top; i <= bottom; i++){
-                for (let j = left; j <= right; j++){
-                    if (numbers[i][j] > 0) fertile++;
-                }
-            }
-            squaresArr.push(square);
-            effectivityArr.push(fertile/square);
-        }
-        let maxEffectivity = 0,
-            maxSize = 0;
-
-        for (let i = 0; i < squaresArr.length; i++){
-            if (effectivityArr[i] >= maxEffectivity && squaresArr[i] > 1){
-                maxEffectivity = effectivityArr[i];
-            }
-        }
-        for (let i = 0; i < squaresArr.length; i++){
-            if (effectivityArr[i] == maxEffectivity && squaresArr[i] >= maxSize && squaresArr[i] > 1){
-                maxSize = squaresArr[i];
-            }
-        }
-
-        console.log(maxSize)
+            return { area: (region.maxX - region.minX + 1) * (region.maxY - region.minY + 1), areaPower };
+        })
+        .map((region) => ({ ...region, soilQuality: region.areaPower / region.area, }))
+        .sort((a, b) => {
+            const soilQualityDiff = b.soilQuality - a.soilQuality;
+            if (soilQualityDiff) return soilQualityDiff;
+        
+            const areaDiff = b.area - a.area;
+            return areaDiff;
+        });
+        const [region] = analytics;
+        console.log(region.area);
         readline.close();
     }
     i++;
-    
 }).on('close', () => process.exit(0));
-
-const neighbors = (arr, m, n, y, x, number) => {
-    arr[y][x] = number;
-    for(let i = Math.max(0, (y - 1)); i <= Math.min((y + 1), (m - 1)); i++){
-        for(let j = Math.max(0, (x - 1)); j <= Math.min((x + 1), (n - 1)); j++){
-            if(arr[i][j] === 1) neighbors(arr, n, m, i, j, number);
-        }    
-    }
-}
